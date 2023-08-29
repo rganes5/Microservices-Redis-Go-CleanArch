f90322c2bae60a372b05b9e29c9e2ba724ff6f9a
Ganesh <ganeshraveendranit@gmail.com>
2023-08-29 22:27:08 +0530

    working on api-gateway

---
+++ api_gateway/pkg/config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Port       string `mapstructure:"PORT"`
+	AuthSvcUrl string `mapstructure:"AUTH_SVC_URL"`
+}
+
+func LoadConfig() (config *Config, err error) {
+	viper.AddConfigPath(".")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error reading config file: %s", err)
+	}
+
+	if err := viper.Unmarshal(&config); err != nil {
+		log.Fatalf("Error unmarshaling config: %s", err)
+	}
+
+	return
+}

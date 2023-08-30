07992e5a8cd8160441110f66cb1453dcf40d8dff
Ganesh <ganeshraveendranit@gmail.com>
2023-08-30 12:21:30 +0530

    working on api-gateway

--- api_gateway/pkg/utils/req.go
+++ api_gateway/pkg/utils/req.go
@@ -7,6 +7,14 @@ type SignUpBody struct {
 	Phone     string `json:"phone" binding:"required"`
 }
 
+type UpdateBody struct {
+	Id        uint32 `json:"id" binding:"required"`
+	FirstName string `json:"firstname" binding:"required"`
+	LastName  string `json:"lastname" binding:"required"`
+	Email     string `json:"email" binding:"required"`
+	Phone     string `json:"phone" binding:"required"`
+}
+
 type Response struct {
 	Response string
 }
